bf5e066b75e8af22335757898a9402fdd7e6635f
phat <nhatphay722@gmail.com>
2023-11-16 18:51:19 +0700

    edit

--- manReader_main/settings.py
+++ manReader_main/settings.py
@@ -140,12 +140,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
-    'manReader_main/static'
+    os.path.join(BASE_DIR, 'static')
 ]
-
 STATICFILES_STORAGE = 'whitenoise.django.CompressedManifestStaticFilesStorage'
 
 # Media files configuration
